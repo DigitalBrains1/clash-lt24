812cc8632281a45fb841de247d8a63deb91be58a
Peter Lebbing <peter@digitalbrains.com>
2015-02-02 03:04:53 +0100

    Update simulateLT24

--- Simul/LT24/LT24.hs
+++ Simul/LT24/LT24.hs
@@ -11,8 +11,8 @@ import Simul.Common
 import LT24.LT24
 
 simulateLT24 :: [(Action, Unsigned 16, Unsigned 16)]
-             -> [ (Bool, Unsigned 16, Bit, Bit, Bit, Bit, Bit, Unsigned 16
-                 , Bit)]
+             -> [ ( Bool, Unsigned 16, Bit, Bit, Bit, Bit, Bit, Bit, Unsigned 16
+                  , Bit)]
 
 simulateLT24 = simulate (pack . lt24 . unpack)
              . (++ repeat (NOP, 0, 0))
