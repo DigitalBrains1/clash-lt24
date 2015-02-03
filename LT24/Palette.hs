e0caa47bb3bbc0cc7257a9aa9e462fb61577415f
Peter Lebbing <peter@digitalbrains.com>
2015-02-03 17:15:11 +0100

    Prettier pal5bTo6b function

--- LT24/Palette.hs
+++ LT24/Palette.hs
@@ -5,14 +5,19 @@ import CLaSH.Prelude
 {-
  - Convert a 5-bit subpixel brightness to a 6-bit brightness
  -
- - Ranges from black to full brightness.
+ - Ranges from black to full brightness. Uses the same trick as the 16-bit RGB
+ - interface from the Ilitek datasheet: put the MSB in the LSB. That way, the
+ - dimmest 16 colors start from black, and the brightest 16 colors end in full
+ - brightness. Brightness 15 becomes 30, brightness 16 becomes 33.
  -}
 
 pal5bTo6b :: Unsigned 5
           -> Unsigned 6
 
 pal5bTo6b 0 = 0
-pal5bTo6b b = fromBV $ (<: H) $ toBV b
+pal5bTo6b b5 = fromBV $ b5b <: vhead b5b
+    where
+        b5b = toBV b5
 
 {-
  - Convert a 6-bit subpixel brightness to a 5-bit brightness
