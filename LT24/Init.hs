719a95ba7389f123c3910e82e1faec90960be788
Peter Lebbing <peter@digitalbrains.com>
2015-02-12 18:23:12 +0100

    Init: CASET, PASET, wipe screen, documentation

--- LT24/Init.hs
+++ LT24/Init.hs
@@ -20,36 +20,46 @@ import Toolbox.Misc
 import Toolbox.FClk
 
 {-
- - Initialise the display controller
+ - Initialize the display controller
  -
- - Initialise the LT24 display controller to accept a 16bpp format, and
- - initialise the palette to a direct color mapping, such that the 5-bits
- - brightness values range from black to full brightness.
+ - Runs through all elements of the initSteps vector. This initializes the
+ - display.
  -
- - It's a simple state machine that passes a list of commands and data to
- - LT24.lt24, waiting for acceptance of the command before passing on to the
- - next. It is designed to be daisy-chained with the component that handles the
+ - Every element in initSteps can be one of:
+ -
+ -      IlL a d        -- Literal: send an action a and din d to lt24.
+ -      IlDoPalette5b  -- Write the 32 palette values that initialise the
+ -                        palette of a subpixel to a direct color mapping.
+ -      IlDoPalette6b  -- Write the 64 palette values for green
+ -      IlWait n       -- Wait for n clockticks
+ -      IlWipeFb       -- Write 320*240 (=76,800) zeroes to the display
+ -
+ - It's a simple state machine walking the vector, executing each element as
+ - indicated above and waiting for acceptance of the command before passing
+ - on to the next.
+ -
+ - It is designed to be daisy-chained with the component that handles the
  - display after initialisation. As long as initialisation is running, the
  - daisy-chained component never sees its command being accepted (by ready going
- - to False). After the initialisation is done, initLt24 is transparent, simply
+ - to False). After the initialization is done, initLt24 is transparent, simply
  - passing the commands and data from the daisy-chained component.
  -
- - `(i, si)` is a pair (index, subindex) that indexes the list of commands to
- - send to LT24.lt24.
+ - `i` indexes initSteps. After running from the highest-numbered, i.e.,
+ - first, element down to 0, there are two further final steps which are
+ - numbered -1 and -2. By writing it as (0 - 1) we can still work with an
+ - Unsigned for i. Step -1 waits for LT24.lt24 to complete, and -2 is the
+ - final state, which is never left, where initLt24 is transparent and
+ - dormant.
  -
- - When `i` reaches 13, initialisation is done. The way `i` = 13 is
- - special-cased in the next-state expression means that the state is quiescent
- - once reaching completion. If it were not special-cased, `ph` would follow
- - the actions of the daisy-chained component, creating useless signal changes
- - in the FPGA.
+ - `si` is "subindex" and keeps track of the progress of the execution of the
+ - current element from initSteps. For instance, it iterates the palette
+ - values, and counts ticks in IlWait.
  -
- - `i` = 10 is handled specially because it is a wait period rather than a
- - command, and `i` = 12 is simply waiting for the last command to complete.
- -
- - 'ph' indicates the phase of command acceptance by LT24.lt24, tracking the
+ - `ph` indicates the phase of command acceptance by LT24.lt24, tracking the
  - `ready` signal.
  -
- - `im` and `ni` stand for "i minus" and "next i" respectively.
+ - Interpretation hint: `im` and `ni` stand for "i minus" and "next i"
+ - respectively.
  -}
 
 initLt24 = initLt24' <^> (fromInteger $ vlength initSteps - 1, 0, L)
@@ -73,6 +83,7 @@ initLt24' (i, si, ph) (ready, actionDaisy, lt24dinDaisy)
                                              . pal5bTo6b
                                              . resize) si)
                IlDoPalette6b -> (LT24.Write, resize si   )
+               IlWipeFb      -> (LT24.Write, 0           )
                _             -> (LT24.NOP  , 0           )
         step = initSteps!i
 
@@ -90,14 +101,15 @@ initLt24' (i, si, ph) (ready, actionDaisy, lt24dinDaisy)
         sip = si + 1
 
         (ni, nsi) = case (step, si) of
-                      (IlL a d      , _ ) -> (im, 0  )
-                      (IlDoPalette5b, 31) -> (im, 0  )
-                      (IlDoPalette6b, 63) -> (im, 0  )
-                      (IlWait n     , _ ) -> if si == n then
-                                               (im, 0  )
-                                             else
-                                               (i , sip)
-                      _                   -> (i , sip)
+                      (IlL a d      , _    ) -> (im, 0  )
+                      (IlDoPalette5b, 31   ) -> (im, 0  )
+                      (IlDoPalette6b, 63   ) -> (im, 0  )
+                      (IlWait n     , _    ) -> if si == n then
+                                                  (im, 0  )
+                                                else
+                                                  (i , sip)
+                      (IlWipeFb     , 76799) -> (im, 0  ) -- 320 * 240 - 1
+                      _                      -> (i , sip)
 
 {-
  - Combines initLt24 and LT24.lt24, with as a result a component with the same
--- LT24/InitSteps.hs
+++ LT24/InitSteps.hs
@@ -12,13 +12,24 @@ import qualified Toolbox.ClockScale as CS
 import Toolbox.Misc
 import Toolbox.FClk
 
-type IlSIndex = $(uToFit $ max (CS.ticksMinPeriod fClk 120e-3) 63)
+type IlSIndex = $(uToFit $ max (CS.ticksMinPeriod fClk 120e-3) (320*240))
 data IlStep = IlL LT24.Action (Unsigned 16) | IlDoPalette5b | IlDoPalette6b
-            | IlWait IlSIndex
+            | IlWait IlSIndex | IlWipeFb
 
+{-
+ - These are the steps necessary to initialize the LT24's controller.
+ -
+ - It is initialized in a 16bpp mode with a direct color mapping. The
+ - coordinates are chosen such that if you have the LT24 module oriented with
+ - the silkscreen text "terasIC LT24" upright, then the coordinates (x,y) are
+ - (0,0) top-left, (319,0) top-right, (0,239) bottom-left and (319,239)
+ - bottom-right.
+ -}
 initSteps =  IlL LT24.Reset   0
           :> IlL LT24.Command cMADCTL
-          :> IlL LT24.Write   232           -- Coordinate system change,
+          :> IlL LT24.Write   232           -- Alternate row address order,
+                                            -- alternate column address order,
+                                            -- row / column exchange
                                             -- BGR subpixel order
           :> IlL LT24.Command cCOLMOD
           :> IlL LT24.Write   5             -- 16bpp MCU interface
@@ -26,6 +37,18 @@ initSteps =  IlL LT24.Reset   0
           :> IlDoPalette5b                  -- Red
           :> IlDoPalette6b                  -- Green
           :> IlDoPalette5b                  -- Blue
+          :> IlL LT24.Command cCASET        -- Since we exchanged rows and
+          :> IlL LT24.Write   0             -- columns, we need to set the
+          :> IlL LT24.Write   0             -- end addresses correctly
+          :> IlL LT24.Write   1
+          :> IlL LT24.Write   0x3F
+          :> IlL LT24.Command cPASET
+          :> IlL LT24.Write   0
+          :> IlL LT24.Write   0
+          :> IlL LT24.Write   0
+          :> IlL LT24.Write   0xEF
+          :> IlL LT24.Command cRAMWR        -- Clear the framebuffer
+          :> IlWipeFb
           :> IlL LT24.Command cSLPOUT
           :> IlWait $(CS.ticksMinPeriodTH fClk 120e-3)
           :> IlL LT24.Command cDISPON
