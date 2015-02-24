b24b034bf21e1437679dd8734ecd6d76e1b9b489
Peter Lebbing <peter@digitalbrains.com>
2015-02-24 17:38:07 +0100

    Made a thinko with framebuffer size

--- LT24/Framebuffer.hs
+++ LT24/Framebuffer.hs
@@ -46,11 +46,11 @@ framebuffer (actionDaisy, dinDaisy, fbAddr, fbDin, fbWrEn, doUpdate, ltdin)
 genCoords (x, y) nextCoords = ((x', y'), (x', y', coordsDone))
     where
         (x', y') | nextCoords = case (x, y) of
-                                  ( 6,  3) -> (0  , 0  )
-                                  ( 6, _ ) -> (0  , y+1)
+                                  ( 7,  3) -> (0  , 0  )
+                                  ( 7, _ ) -> (0  , y+1)
                                   ( _, _ ) -> (x+1, y  )
                  | otherwise  = (x, y)
-        coordsDone = (x, y) == ( 6,  3)
+        coordsDone = (x, y) == ( 7,  3)
 
 ramAddr :: (Unsigned 6, Unsigned 6)
           -> Unsigned 12
